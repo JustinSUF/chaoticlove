246c2aa1939ed4edc44955718b877a2f5b8b4692
MohamedMediouni1 <147090444+MohamedMediouni1@users.noreply.github.com>
2024-05-20 13:50:18 -0400

    Game menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
